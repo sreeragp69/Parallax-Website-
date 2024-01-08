b1cfefcb95866bba05d1773de9323c560a7e56cb
Sreerag P <103045973+sreeragp69@users.noreply.github.com>
2024-01-08 18:28:24 +0530

    first commit

---
+++ src/TextBlock.jsx
@@ -0,0 +1,29 @@
+function TextBlock() {
+  return (
+    <div id="textblock">
+      <div id="textblock-container">
+        <h1 id="textblock-title">Lorem, ipsum dolor.</h1>
+        <p id="textblock-content">
+          Lorem ipsum dolor sit.
+          <br />
+          <br />
+          adipisicing elit. Modi odit vitae fugit doloribus.
+          Lorem ipsum dolor sit amet cisicing el's your job to find smoke a
+          <br />
+          <br />
+          sit amet consectetur, adipisicing elit. Modi odit vitae fugit doloribus.
+          Lorem ipsum dolor sit amet cisicing elit. Modi odit vitae fugit doloribus.isicing elit. Modi odit vitae fugit doloribus.
+          behind.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi odit vitae fugit doloribus.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi odit vitae fugit doloribus.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi odit vitae fugit doloribus.
+        </p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default TextBlock;
